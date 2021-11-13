48e79c96f215abd964bd737e6197af1c61c9379d
Jorge Alberto Alberto Arroyo Esquivel <jarroyoe@farm.cse.ucdavis.edu>
2021-11-13 10:04:41 -0800

    debug

--- train.jl
+++ train.jl
@@ -2,7 +2,7 @@ using DelimitedFiles, BSON, CUDA
 using Flux, Statistics, StatsBase, Random
 using Printf, Plots
 using Base.Iterators: repeated, partition
-CUDA.allowscalar(true)
+#CUDA.allowscalar(true)
 
 #Load CSVs
 @info("Loading data set")
@@ -12,11 +12,11 @@ test_train = collect(partition(random_split,Int(ceil(length(random_split)*train_
 
 maxTemperature = 30
 
-train_lr = [readdlm("./data/ANOBIG smaller_region/anobig_temp"*string(i)*".txt",',') for i in test_train[1]]/maxTemperature
-test_lr = [readdlm("./data/ANOBIG smaller_region/anobig_temp"*string(i)*".txt",',') for i in test_train[2]]/maxTemperature
+train_lr = [readdlm("./data/ANOBIG smaller_region/anobig_temp"*string(i)*".txt",',',Float32) for i in test_train[1]]/maxTemperature
+test_lr = [readdlm("./data/ANOBIG smaller_region/anobig_temp"*string(i)*".txt",',',Float32) for i in test_train[2]]/maxTemperature
 
-train_hr = [readdlm("./data/ANOHIGH smaller_region/anohigh_temp"*string(i)*".txt",',') for i in test_train[1]]/maxTemperature
-test_hr = [readdlm("./data/ANOHIGH smaller_region/anohigh_temp"*string(i)*".txt",',') for i in test_train[2]]/maxTemperature
+train_hr = [readdlm("./data/ANOHIGH smaller_region/anohigh_temp"*string(i)*".txt",',',Float32) for i in test_train[1]]/maxTemperature
+test_hr = [readdlm("./data/ANOHIGH smaller_region/anohigh_temp"*string(i)*".txt",',',Float32) for i in test_train[2]]/maxTemperature
 
 #Bundle training data into batches
 function make_minibatch(X, Y, idxs)
@@ -28,7 +28,7 @@ function make_minibatch(X, Y, idxs)
     end
     return (X_batch, Y_batch)
 end
-batch_size = 10
+batch_size = 16
 mb_idxs = partition(1:length(train_lr), batch_size)
 train_set = [make_minibatch(train_lr, train_hr, i) for i in mb_idxs]
 
@@ -44,25 +44,25 @@ hr_size_y = size(train_set[1][2],2)
 @info("Constructing model...")
 model = Chain(
     Conv((9,9),1=>64,pad=(1,1),relu),
-    x -> maxpool(x,(1,1)),
+    #x -> maxpool(x,(1,1)),
     Dropout(0.4),
 
     Conv((3,3),64=>32,pad=(30,30),relu),
-    x -> maxpool(x,(1,1)),
+    #x -> maxpool(x,(1,1)),
     Dropout(0.4),
 
-    Conv((5,4),32=>1,pad=(0,4),sigmoid),
-    x -> maxpool(x,(1,1)),
-    Dropout(0.4),
+    Conv((5,4),32=>1,pad=(0,4)),
+    #x -> maxpool(x,(1,1)),
+    #Dropout(0.4),
 
     x -> reshape(x,:,size(x,4))
     #Dense(99,hr_size_x*hr_size_y)
 )
 
 # Load model and datasets onto GPU, if enabled
-train_set = gpu.(train_set)
-test_set = gpu.(test_set)
-model = gpu(model)
+#train_set = gpu.(train_set)
+#test_set = gpu.(test_set)
+#model = gpu(model)
 
 # Make sure our model is nicely precompiled before starting our training loop
 model(train_set[1][1])
@@ -72,12 +72,14 @@ function loss(x,y)
     x_aug = x .+ 0.1f0*gpu(randn(eltype(x), size(x)))
 
     y_hat = model(x_aug)
-    y = CuArray(reshape(y,(hr_size_x*hr_size_y,size(x,4))))
-    return psnr(y_hat,y,1)
+    y_t = #CuArray
+	reshape(y,(hr_size_x*hr_size_y,size(x,4)))
+    return Float32(msd(y_hat,y_t))
 end
 function accuracy(x,y)
-    y = CuArray(reshape(y,(hr_size_x*hr_size_y,length(test_train[2]))))
-    psnr(model(x),y,1)
+    y_t = #CuArray
+	reshape(y,(hr_size_x*hr_size_y,length(test_train[2])))
+    Float32(msd(model(x),y_t))
 end
 
 opt = ADAM(0.001)
@@ -85,20 +87,24 @@ opt = ADAM(0.001)
 @info("Beginning training loop...")
 best_acc = Inf
 last_improvement = 0
+train_error = []
+test_error = []
 @time for epoch_idx in 1:100
     global best_acc, last_improvement
     # Train for a single epoch
     Flux.train!(loss, params(model), train_set, opt)
+    append!(train_error,loss(train_set[1]...))
 
     # Calculate accuracy:
     acc = accuracy(test_set...)
+    append!(test_error,acc)
     @info(@sprintf("[%d]: Test accuracy: %.4f", epoch_idx, acc))
     
     #If our accuracy is good enough, quit out.
-    if acc <= 0.05
-        @info(" -> Early-exiting: We reached our target accuracy of 95%")
-        break
-    end
+   # if acc <= 0.05
+   #     @info(" -> Early-exiting: We reached our target accuracy of 95%")
+   #     break
+   # end
 
     # If this is the best accuracy we've seen so far, save the model out
     if acc <= best_acc
@@ -124,6 +130,14 @@ last_improvement = 0
 end
 
 #first_predict = Array(reshape(model(test_set[1])[:,1],(hr_size_x,hr_size_y)))
-#first_truth = Array(test_set[2][:,:,:,1])
+first_truth = Array(test_set[2][:,:,:,1])
 #display(heatmap(first_truth))
 #display(heatmap(first_predict))
+
+first_predict = Array(reshape(model(test_set[1])[:,1],(hr_size_x,hr_size_y)))
+#first_truth = Array(reshape(test_set[2][:,:,:,1]),(hr_size_x,hr_size_y))
+
+writedlm("first_predict.csv",first_predict)
+writedlm("first_truth.csv",first_truth)
+writedlm("test_error.csv",test_error)
+writedlm("train_error.csv",train_error)
